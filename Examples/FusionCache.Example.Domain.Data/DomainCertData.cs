760b957b39d2323a0b8d509820a90337f4cd7df4
Joe Shook <joeshook@gmail.com>
2021-06-04 13:17:53 -0700

    Retain work

---
+++ Examples/FusionCache.Example.Domain.Data/DomainCertData.cs
@@ -0,0 +1,18 @@
+﻿namespace FusionCache.Example.Domain.Model
+{
+    /// <summary>
+    /// Mock data view after some database joins
+    /// </summary>
+    public class DomainCertData
+    {
+        public int id { get; set; }
+
+        public string domain { get; set; }
+
+        /// <summary>
+        /// Data is fake.  Not a real certificate
+        /// </summary>
+        public string certificate { get; set; }
+        
+    }
+}
---
+++ Examples/FusionCache.Example.Domain.Data/EmailToIpData.cs
@@ -0,0 +1,18 @@
+﻿namespace FusionCache.Example.Domain.Model
+{
+    /// <summary>
+    /// Mock data view after some interesting network resolution from domain to
+    /// MX record to A record with a ttl (time to live)
+    /// This is a simple view that only has one IP.
+    /// </summary>
+    public class EmailToIpData
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string SmtpIp { get; set; }
+
+        public int Ttl { get; set; }
+    }
+}
