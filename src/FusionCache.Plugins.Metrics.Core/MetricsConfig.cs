67042f0655af7c0258e689af2cc905d52d800d10
Joseph Shook <Joseph.Shook@Surescripts.com>
2022-02-19 15:31:52 -0800

    Adding code comments.

--- src/FusionCache.Plugins.Metrics.Core/MetricsConfig.cs
+++ src/FusionCache.Plugins.Metrics.Core/MetricsConfig.cs
@@ -2,11 +2,29 @@
 
 namespace ZiggyCreatures.Caching.Fusion.Plugins.Metrics.Core
 {
+    /// <summary>
+    /// Helper model for accessing ApplicationName, ApplicationVersion, metric Prefix and metrics default MeasurementName.
+    /// </summary>
     public class MetricsConfig
     {
-        public string ApplicationName { get; set; } = Assembly.GetEntryAssembly().GetName().Name;
-        public string ApplicationVersion { get; set; } = Assembly.GetEntryAssembly().ImageRuntimeVersion;
+        /// <summary>
+        /// Set ApplicationName or let it discover it from the entry assembly.
+        /// </summary>
+        public string ApplicationName { get; set; } = Assembly.GetEntryAssembly()?.GetName().Name;
+
+        /// <summary>
+        /// Set ApplicationVersion or let it discover it from the entry assembly's ImageRuntimeVersion.
+        /// </summary>
+        public string ApplicationVersion { get; set; } = Assembly.GetEntryAssembly()?.ImageRuntimeVersion;
+        
+        /// <summary>
+        /// Prefix for all metrics written
+        /// </summary>
         public string Prefix { get; set; } = "appMetrics";
+
+        /// <summary>
+        /// Consistent MeasurementName for all cache metrics written.
+        /// </summary>
         public string MeasurementName { get; set; } = "Cache-Events";
     }
 }
--- src/FusionCache.Plugins.Metrics.Core/SemanticConventions.cs
+++ src/FusionCache.Plugins.Metrics.Core/SemanticConventions.cs
@@ -1,17 +1,30 @@
 ﻿namespace ZiggyCreatures.Caching.Fusion.Plugins.Metrics.Core
 {
+    /// <summary>
+    /// Default implementation of <see cref="ISemanticConventions"/> 
+    /// </summary>
     public class SemanticConventions : ISemanticConventions
     {
-        public SemanticConventions(string measurementName, string GaugeName)
+        /// <summary>
+        /// Create a new SemanticConventions instance
+        /// </summary>
+        public SemanticConventions(string measurementName, string gaugeName)
         {
             _measurementName = measurementName;
-            _gaugeName = GaugeName;
+            _gaugeName = gaugeName;
         }
 
+        /// <summary>
+        /// Create a new SemanticConventions instance
+        /// </summary>
         public SemanticConventions()
         {
         }
 
+        /// <summary>
+        /// Static accessor to creating a new instance of <see cref="SemanticConventions"/>
+        /// </summary>
+        /// <returns></returns>
         public static SemanticConventions Instance()
         {
             return new SemanticConventions();
@@ -20,36 +33,64 @@
         private readonly string _measurementName = "Cache.Events";
         private readonly string _gaugeName = "Cache.Gauges";
 
+        /// <inheritdoc />
         public string MeasurementName => _measurementName;
 
+        /// <inheritdoc />
         public string GaugeName => _gaugeName;
 
+        /// <inheritdoc />
         public string ValueFieldName => "value";
 
+        /// <inheritdoc />
         public string ApplicationTagName => "application";
 
+        /// <inheritdoc />
         public string ApplicationVersionTagName => "applicationVersion";
 
+        /// <inheritdoc />
         public string CacheNameTagName => "cacheName";
 
+        /// <inheritdoc />
         public string CacheEventTagName => "cacheEvent";
 
+        /// <inheritdoc />
         public string CacheHitTagValue => "HIT";
 
+        /// <inheritdoc />
         public string CacheMissTagValue => "MISS";
 
+        /// <inheritdoc />
         public string CacheSetTagValue => "SET";
 
+        /// <inheritdoc />
         public string CacheStaleHitTagValue => "STALE_HIT";
 
+        /// <inheritdoc />
         public string CacheBackgroundRefreshedTagValue => "STALE_REFRESH";
+
+        /// <inheritdoc />
         public string CacheBackgroundFailedRefreshedTagValue => "STALE_REFRESH_ERROR";
+
+        /// <inheritdoc />
         public string CacheExpiredEvictTagValue => "EXPIRE";
+
+        /// <inheritdoc />
         public string CacheCapacityEvictTagValue => "CAPACITY";
+
+        /// <inheritdoc />
         public string CacheRemovedTagValue => "REMOVE";
+
+        /// <inheritdoc />
         public string CacheItemCountTagValue => "ITEM_COUNT";
+
+        /// <inheritdoc />
         public string CacheCacheFactoryErrorTagValue => "FACTORY_ERROR";
+
+        /// <inheritdoc />
         public string CacheFactorySyntheticTimeoutTagValue => "TIMEOUT";
+
+        /// <inheritdoc />
         public string CacheFailSafeActivateTagValue => "FAIL_SAFE";
     }
 }
