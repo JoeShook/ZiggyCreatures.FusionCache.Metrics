17038dbc78941c2fb7481828437403640eda2015
Joseph Shook <Joseph.Shook@Surescripts.com>
2022-04-26 15:53:44 -0700

    Package updates

--- examples/AppMetricsPluginExampleFramework/Areas/HelpPage/SampleGeneration/InvalidSample.cs
+++ examples/AppMetricsPluginExampleFramework/Areas/HelpPage/SampleGeneration/InvalidSample.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace AppMetricsPluginExample.Areas.HelpPage.SampleGeneration
+namespace AppMetricsPluginExample.Areas.HelpPage
 {
     /// <summary>
     /// This represents an invalid sample on the help page. There's a display template named InvalidSample associated with this class.
