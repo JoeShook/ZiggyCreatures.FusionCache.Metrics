93b00a3faaf71047ecc2b4d46d6139bda0f04a16
Joseph Shook <Joseph.Shook@Surescripts.com>
2022-04-28 17:08:46 -0700

    Benchmarks added
    Similar results to EventCounters

---
+++ benchmarks/FusionCache.Plugins.Metrics.OpenTelemetry.Benchmarks/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly)
+    .Run(
+        args 
+        // , new DebugInProcessConfig()
+        );
---
+++ benchmarks/FusionCache.Plugins.Metrics.OpenTelemetry.Benchmarks/TestExporter.cs
@@ -0,0 +1,21 @@
+﻿using OpenTelemetry;
+
+namespace ZiggyCreatures.Fusion.Caching.Plugins.OpenTelemetry.Benchmarks
+{
+    internal class TestExporter<T> : BaseExporter<T> where T : class
+    {
+        private readonly Action<Batch<T>> processBatchAction;
+
+        public TestExporter(Action<Batch<T>> processBatchAction)
+        {
+            this.processBatchAction = processBatchAction ?? throw new ArgumentNullException(nameof(processBatchAction));
+        }
+
+        public override ExportResult Export(in Batch<T> batch)
+        {
+            this.processBatchAction(batch);
+
+            return ExportResult.Success;
+        }
+    }
+}
